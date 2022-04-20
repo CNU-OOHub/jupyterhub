7c779d1269972f03548f06fd2434203c12f1332a
sonyi00 <ksl2950@naver.com>
2022-04-20 21:21:00 +0900

    delete all

--- jupyterhub-1/jupyterhub/apihandlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import auth, groups, hub, proxy, services, users
-from .base import *
-
-default_handlers = []
-for mod in (auth, hub, proxy, users, groups, services):
-    default_handlers.extend(mod.default_handlers)
